2ad21bc8d24f4cc391969389105f22301d4cd430
Priyanshu <priyanshudeep001@gmail.com>
2023-07-03 16:36:24 +0530

    removed windows stuff

--- PowerShell/search.ps1
+++
@@ -1,8 +0,0 @@
-$targetName = Read-Host "Enter name"
-
-$foundPaths = Get-ChildItem -Path "." -Recurse -Filter "*$targetName*"
-
-if ($foundPaths) {
- $foundPaths | ForEach-Object { Write-Host "Found: $($_.FullName)" }
-                 }
-else { Write-Host "N/A" -ForegroundColor Red}
